91694da528f4bb2b8937bb5886fe71ebcf3a232a
DiogoGabriel <diogogabriel.tech@gmail.com>
2025-02-19 18:45:26 -0300

    Adding script in page

---
+++ script.js
@@ -0,0 +1,85 @@
+let currentPageUrl = 'https://swapi.dev/api/people/';
+
+window.onload = async () => {
+    try {
+        await loadCharacters(currentPageUrl)
+    } catch (error) {
+        console.log(error);
+        alert('Erro ao carregar cards');
+    }
+
+    const nextButton = document.getElementById('next-button');
+    const backButton = document.getElementById('back-button');
+
+    nextButton.addEventListener('click', loadNextPage)
+    backButton.addEventListener('click', loadPreviousPage)
+};
+
+async function loadCharacters(url) {
+    const mainContent = document.getElementById('main-content');
+    mainContent.innerHTML = '';
+
+    try {
+        const response = await fetch(url);
+        const responseJson = await response.json();
+
+        responseJson.results.forEach((character) => {
+            const card = document.createElement('div')
+            card.style.backgroundImage = `url('./assets/img/people/${character.url.replace(/\D/g, "")}.jpg')`;
+            card.className = 'cards';
+
+            const characterNameBg = document.createElement('div');
+            characterNameBg.className = 'character-name-bg';
+
+            const charactername = document.createElement('span');
+            charactername.className = 'character-name'
+            charactername.innerText =  `${character.name}`
+
+            characterNameBg.appendChild(charactername);
+            card.appendChild(characterNameBg);
+
+            mainContent.appendChild(card);
+        });
+
+        const nextButton = document.getElementById('next-button');
+        const backButton = document.getElementById('back-button');
+
+        nextButton.disabled = !responseJson.next;
+        backButton.disabled = !responseJson.previous;
+        
+        backButton.style.visibility = responseJson.previous? "visible" : "hidden";
+
+        currentPageUrl = url
+    } catch (error) {
+        console.log(error);
+        alert('Erro ao carregar personagens')
+    }
+}
+
+async function loadNextPage() {
+    if (!currentPageUrl) return;
+
+    try {
+        const response = await fetch(currentPageUrl);
+        const responseJson = await response.json();
+
+        await loadCharacters(responseJson.next)
+    } catch (error) {
+        console.log(error)
+        alert('Erro ao carregar a próxima página')
+    }
+}
+
+async function loadPreviousPage() {
+    if (!currentPageUrl) return;
+
+    try {
+        const response = await fetch(currentPageUrl);
+        const responseJson = await response.json();
+
+        await loadCharacters(responseJson.previous)
+    } catch (error) {
+        console.log(error)
+        alert('Erro ao carregar a página anterior')
+    }
+}
